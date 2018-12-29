ed5747108479074f372929d2cbe9150041ac4512
Nikhil Vytla <nvawesomeguy@gmail.com>
2018-12-29 06:19:26 +0000

    Added form validations to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,7 @@
 # An object is basically a list of attributes (imagine key-value pairs).
 # Model files are where we place the main blueprint of the object (including parameters, attributes, etc.)
 # Rails takes care of the object blueprints via the schema.rb
+# Check out https://guides.rubyonrails.org/active_record_validations.html for more info on active record validations.
 class Contact < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
