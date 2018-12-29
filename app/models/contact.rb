c6ca1aff6ea874109a7dace1f2e33b4983a6fa17
Nikhil Vytla <nvawesomeguy@gmail.com>
2018-12-29 05:36:42 +0000

    Added contact model file

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
+# You add model files for any objects that you want your project to be working on.
+# An object is basically a list of attributes (imagine key-value pairs).
+# Model files are where we place the main blueprint of the object (including parameters, attributes, etc.)
+# Rails takes care of the object blueprints via the schema.rb
 class Contact < ActiveRecord::Base
+  
 end
