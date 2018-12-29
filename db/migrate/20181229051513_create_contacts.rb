37cb7c3a6c21d8eff1ffa5953ca3482dd698e1af
Nikhil Vytla <nvawesomeguy@gmail.com>
2018-12-29 05:26:09 +0000

    Created contacts table

--- db/migrate/20181229051513_create_contacts.rb
+++ db/migrate/20181229051513_create_contacts.rb
@@ -1,3 +1,4 @@
+# Check out https://edgeguides.rubyonrails.org/active_record_migrations.html for more info on active record migrations.
 class CreateContacts < ActiveRecord::Migration[5.0]
   def change
     create_table :contacts do |t|
