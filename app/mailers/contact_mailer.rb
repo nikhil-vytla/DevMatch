73b5710ff9d6fe9b564d9cc7bf2cbb0a46af5007
Nikhil Vytla <nvawesomeguy@gmail.com>
2018-12-29 23:05:03 +0000

    Added contact form mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,10 @@
+class ContactMailer < ActionMailer::Base
+  default to: 'cnikhil.vytla@gmail.com'
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from: email, subject: 'Contact Form Message')
+  end
+end
