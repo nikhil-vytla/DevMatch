1adb1538e3e0f0a9de08220e8062c2f74f73ff0f
Nikhil Vytla <nvawesomeguy@gmail.com>
2018-12-29 05:10:31 +0000

    Added nav link for contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+# Check out https://guides.rubyonrails.org/routing.html for details on CRUD and RESTful routes
 Rails.application.routes.draw do
     root to: "pages#home"
     get 'about', to: "pages#about"
