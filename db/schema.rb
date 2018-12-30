18d50b8ecf398b6437484009e790f75e9266aa01
Nikhil Vytla <nvawesomeguy@gmail.com>
2018-12-30 05:35:49 +0000

    Added profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181230041418) do
+ActiveRecord::Schema.define(version: 20181230053249) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 20181230041418) do
     t.decimal  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
   end
 
   create_table "users", force: :cascade do |t|
