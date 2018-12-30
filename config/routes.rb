529caafd00c121229d178a56ddc85726e5d2a10a
Nikhil Vytla <nvawesomeguy@gmail.com>
2018-12-30 01:45:07 +0000

    Installed devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # Check out https://guides.rubyonrails.org/routing.html for details on CRUD and RESTful routes
 Rails.application.routes.draw do
+  devise_for :users
     root to: "pages#home"
     get 'about', to: "pages#about"
     resources :contacts, only: :create
