f2a1218114d69eb941c849e9a45577b72919067e
Nikhil Vytla <nvawesomeguy@gmail.com>
2018-12-30 04:57:04 +0000

    Added server side code for creating subscriptions for pro users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ApplicationRecord
   
   # Check out https://guides.rubyonrails.org/association_basics.html for more information on active record associations.
   belongs_to :plan
+  
+  attr_accessor :stripe_card_token
+  def save_with_subscription
+    if valid?
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      self.stripe_customer_token = customer.id
+      save!
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # Check out https://guides.rubyonrails.org/routing.html for details on CRUD and RESTful routes
 Rails.application.routes.draw do
-  devise_for :users
-    root to: "pages#home"
-    get 'about', to: "pages#about"
-    resources :contacts, only: :create
-    get 'contact-us', to: 'contacts#new', as: 'new_contact'
+  root to: "pages#home"
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  get 'about', to: "pages#about"
+  resources :contacts, only: :create
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
