f81964ea1250e9e5cec55f32b11fc2c16cfe7638
Nikhil Vytla <nvawesomeguy@gmail.com>
2018-12-30 03:12:26 +0000

    Added association for plans and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  
+  # Check out https://guides.rubyonrails.org/association_basics.html for more information on active record associations.
   belongs_to :plan
 end
