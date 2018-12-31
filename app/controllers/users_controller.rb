e46fca39ab616d86b67b1a02d6abf2e9c4811b03
Nikhil Vytla <nvawesomeguy@gmail.com>
2018-12-31 03:25:50 +0000

    Added users index view and action details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    # Check out https://guides.rubyonrails.org/active_record_querying.html for more information.
+    @users = User.includes(:profile)
   end
   
   # GET to /users/:id
